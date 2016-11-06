1f46fccec2bf2b226a9be8ffd5a8d6d4daf27f04
Trey Hakanson <treyhakanson@gmail.com>
2016-11-06 14:42:01 -0500

    added structure for api (just to local storage for now, but good to keep things separate)

---
+++ src/api/index.js
@@ -0,0 +1,1 @@
+// used for calling to localStorage
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'React';
+import React, { Component } from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from 'App';
